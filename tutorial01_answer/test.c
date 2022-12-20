f9f642f7fe189300d9394df33d1dfa221480b150
woodcore <zwoodcorea@gmail.com>
2022-12-20 19:29:02 +0800

    解析最后一部分空白，加入true和false的解析和测试

---
+++ tutorial01_answer/test.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "woodjson.h"
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+
+// 反斜线代表该行未结束，会串接下一行。
+// 如果宏里有多过一个语句（statement），就需要用 do {/*...*/} while(0) 包裹成单个语句
+#define EXPECT_EQ_BASE(equality, expect, actual, format) \
+    do {\
+        test_count++;\
+        if (equality)\
+            test_pass++;\
+        else {\
+            fprintf(stderr, "%s:%d: expect: " format "actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+            main_ret = 1;\
+        }\
+    } while(0)
+
+// 提供了一个 EXPECT_EQ_INT(expect, actual) 的宏，每次使用这个宏时，如果 expect != actual （预期值不等于实际值），便会输出错误信息。
+#define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+
+// 测试 null = "null"
+static void test_parse_null() {
+    wood_value v;
+    v.type = WOOD_FALSE;
+    EXPECT_EQ_INT(WOOD_PARSE_OK, wood_parse(&v, "null"));
+    EXPECT_EQ_INT(WOOD_NULL, wood_get_type(&v));
+}
+
+// 测试 null = "true"
+static void test_parse_true() {
+    wood_value v;
+    v.type = WOOD_FALSE;
+    EXPECT_EQ_INT(WOOD_PARSE_OK, wood_parse(&v, "true"));
+    EXPECT_EQ_INT(WOOD_TRUE, wood_get_type(&v));
+}
+
+// 测试 null = "false"
+static void test_parse_false() {
+    wood_value v;
+    v.type = WOOD_TRUE;
+    EXPECT_EQ_INT(WOOD_PARSE_OK, wood_parse(&v, "false"));
+    EXPECT_EQ_INT(WOOD_FALSE, wood_get_type(&v));
+}
+
+// 测试返回值(json 只含有空白) WOOD_PARSE_EXPECT_VALUE
+static void test_parse_expect_value() {
+    wood_value v;
+    
+    v.type = WOOD_FALSE;
+    EXPECT_EQ_INT(WOOD_PARSE_EXPECT_VALUE, wood_parse(&v, ""));
+    EXPECT_EQ_INT(WOOD_NULL, wood_get_type(&v));
+
+    EXPECT_EQ_INT(WOOD_PARSE_EXPECT_VALUE, wood_parse(&v, " "));
+    EXPECT_EQ_INT(WOOD_NULL, wood_get_type(&v));
+}
+
+// 测试返回值(值不是那三种字面值) WOOD_PARSE_INVALID_VALUE
+static void test_parse_invalid_value() {
+    wood_value v;
+    v.type = WOOD_FALSE;
+    EXPECT_EQ_INT(WOOD_PARSE_INVALID_VALUE, wood_parse(&v, "nul"));
+    EXPECT_EQ_INT(WOOD_NULL, wood_get_type(&v));
+
+    v.type = WOOD_FALSE;
+    EXPECT_EQ_INT(WOOD_PARSE_INVALID_VALUE, wood_parse(&v, "?"));
+    EXPECT_EQ_INT(WOOD_NULL, wood_get_type(&v));  
+}
+
+// 测试返回值(最后一个空白之后还有值) WOOD_PARSE_ROOT_NOT_SINGULAR
+static void test_parse_root_not_singular() {
+    wood_value v;
+    v.type = WOOD_FALSE;
+    EXPECT_EQ_INT(WOOD_PARSE_ROOT_NOT_SINGULAR, wood_parse(&v, "null x"));
+    EXPECT_EQ_INT(WOOD_NULL, wood_get_type(&v));
+}
+
+static void test_parse() {
+    test_parse_null();
+    test_parse_true();
+    test_parse_false();
+    test_parse_expect_value();
+    test_parse_invalid_value();
+    test_parse_root_not_singular();
+}
+
+int main() {
+    test_parse();
+    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+    return main_ret;
+}
---
+++ tutorial01_answer/woodjson.c
@@ -0,0 +1,92 @@
+#include "woodjson.h"
+#include <assert.h>  /* assert() */
+#include <stdlib.h>  /* NULL */
+
+// 如果宏里有多过一个语句（statement），就需要用 do {/*...*/} while(0) 包裹成单个语句
+// 如果错误是由于程序员错误编码所造成的（例如传入不合法的参数），那么应用断言
+// 如果错误是程序员无法避免，而是由运行时的环境所造成的，就要处理运行时错误（例如开启文件失败）
+#define EXPECT(c, ch)    do { assert(*c->json == (ch)); c->json++; } while(0)
+
+// 为了减少解析函数之间传递多个参数，我们把这些数据都放进一个 wood_context 结构体：
+typedef struct
+{
+    const char* json;
+}wood_context;
+
+// 解析空白且剔除空白（通过移动指针p），由于这个函数是不会出现错误的，返回类型为 void。其他的解析函数会返回错误码，传递至顶层。
+// ws = *(%x20 / %x09 / %x0A / %x0D)
+//       空格符 / 制表符 / 换行符 / 回车符
+static void wood_parse_whitespace(wood_context* c) {
+    const char *p = c->json;
+    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
+        p++;
+    c->json = p;
+}
+
+// 解析 null
+/* null = "null" */
+static int wood_parse_null(wood_context* c, wood_value* v) {
+    EXPECT(c, 'n');  // 利用宏（宏里面利用断言）判断第一个字符是否为 'n'，并跳到下一字符
+    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+        return WOOD_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = WOOD_NULL;
+    return WOOD_PARSE_OK;
+}
+
+// 解析 true
+/* null = "true" */
+static int wood_parse_true(wood_context* c, wood_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return WOOD_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = WOOD_TRUE;
+    return WOOD_PARSE_OK;
+}
+
+// 解析 false
+/* null = "false" */
+static int wood_parse_false(wood_context* c, wood_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return WOOD_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = WOOD_FALSE;
+    return WOOD_PARSE_OK;
+}
+
+// 解析值，根据剔除空白之后的第一个字符来决定怎么解析
+static int wood_parse_value(wood_context* c, wood_value* v) {
+    switch (*c->json) {
+        case 'n':  return wood_parse_null(c, v);
+        case 't':  return wood_parse_true(c, v);
+        case 'f':  return wood_parse_false(c, v);
+        case '\0': return WOOD_PARSE_EXPECT_VALUE;  // 一个 JSON 只含有空白
+        default:   return WOOD_PARSE_INVALID_VALUE;
+    }
+}
+
+/* 提示：这里应该是 JSON-text = ws value ws */
+/* 以下实现没处理最后的 ws 和 WOOD_PARSE_ROOT_NOT_SINGULAR，所以关于 WOOD_PARSE_ROOT_NOT_SINGULAR 这个返回值的测试用例会过不了 */
+/* 现在加上最后的 ws（即，解析第三部分的空白），加上之后测试用例则可以全部通过，可以和tutorial01进行对比 */
+int wood_parse(wood_value* v, const char* json) {
+    wood_context c;
+    int ret;  // 声明了一个 int 型变量
+    assert(v != NULL);
+    c.json = json;
+    v->type = WOOD_NULL;
+    wood_parse_whitespace(&c);  // 解析空白
+    if ((ret = wood_parse_value(&c, v)) == WOOD_PARSE_OK) {  // 解析值
+        wood_parse_whitespace(&c);  // 如果可以解析出完整的有效的值，则再解析值后面是否是空白
+        if (*c.json != '\0') {  // 如果解析完空白之后，后面的字符不是空字符'\0'的话，则说明后面还有其他值
+            ret = WOOD_PARSE_ROOT_NOT_SINGULAR;  // 返回值设置为 WOOD_PARSE_ROOT_NOT_SINGULAR
+        }
+    }
+    return ret;
+}
+
+wood_type wood_get_type(const wood_value* v) {
+    assert(v != NULL);
+    return v->type;
+}
